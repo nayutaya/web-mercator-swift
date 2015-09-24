2f17a0364ecd6fc08827a7f71cda7e2f592ddaae
Yuya Kato <yuyakato@gmail.com>
2015-09-24 20:10:53 +0900

    Initial Commit

---
+++ WebMercator/WebMercator.h
@@ -0,0 +1,19 @@
+//
+//  WebMercator.h
+//  WebMercator
+//
+//  Created by Yuya Kato on 2015/09/24.
+//  Copyright © 2015年 Nayutaya Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebMercator.
+FOUNDATION_EXPORT double WebMercatorVersionNumber;
+
+//! Project version string for WebMercator.
+FOUNDATION_EXPORT const unsigned char WebMercatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebMercator/PublicHeader.h>
+
+
