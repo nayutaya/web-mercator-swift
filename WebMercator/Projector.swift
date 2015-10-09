c216908ce5ea03c716c1c05c927925793204447f
Yuya Kato <yuyakato@gmail.com>
2015-10-09 18:06:43 +0900

    アクセス修飾子を明示化

--- WebMercator/Projector.swift
+++ WebMercator/Projector.swift
@@ -1,27 +1,27 @@
 
-class Projector {
+public class Projector {
     // 度をラジアンに変換するための係数
-    static let DEGREE_TO_RADIAN = M_PI / 180.0
+    private static let DEGREE_TO_RADIAN = M_PI / 180.0
     // ラジアンを度に変換するための係数
-    static let RADIAN_TO_DEGREE = 180.0 / M_PI
+    private static let RADIAN_TO_DEGREE = 180.0 / M_PI
     
     // 緯度をメルカトルY座標に変換する
-    class func latitudeToMercatorY(latitudeInDegrees: Double) -> Double {
+    public class func latitudeToMercatorY(latitudeInDegrees: Double) -> Double {
         return atanh(sin(latitudeInDegrees * DEGREE_TO_RADIAN)) / M_PI
     }
     
     // 経度をメルカトルX座標に変換する
-    class func longitudeToMercatorX(longitudeInDegrees: Double) -> Double {
+    public class func longitudeToMercatorX(longitudeInDegrees: Double) -> Double {
         return longitudeInDegrees / 180.0
     }
     
     // メルカトルY座標を緯度に変換する
-    class func mercatorYToLatitude(mercatorY: Double) -> Double {
+    public class func mercatorYToLatitude(mercatorY: Double) -> Double {
         return asin(tanh(mercatorY * M_PI)) * RADIAN_TO_DEGREE
     }
     
     // メルカトルX座標を経度に変換する
-    class func mercatorXToLongitude(mercatorX: Double) -> Double {
+    public class func mercatorXToLongitude(mercatorX: Double) -> Double {
         var x = mercatorX
         while ( x < -1.0 ) { x += 2.0 }
         while ( x > +1.0 ) { x -= 2.0 }
