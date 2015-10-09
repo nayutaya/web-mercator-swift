23b53846614a99e8e478d9141b1c86e2bc6ff121
Yuya Kato <yuyakato@gmail.com>
2015-10-09 17:23:52 +0900

    定数を追加

--- WebMercator/WebMercator.swift
+++ WebMercator/WebMercator.swift
@@ -1,3 +1,9 @@
 
 class WebMercator {
+    static let LATITUDE_MAXIMUM_IN_DEGREES = Projector.mercatorYToLatitude(+1.0)
+    static let LATITUDE_MINIMUM_IN_DEGREES = Projector.mercatorYToLatitude(-1.0)
+    static let LATITUDE_DELTA_IN_DEGREES = LATITUDE_MAXIMUM_IN_DEGREES - LATITUDE_MINIMUM_IN_DEGREES
+    static let LONGITUDE_MAXIMUM_IN_DEGREES = Projector.mercatorXToLongitude(+1.0)
+    static let LONGITUDE_MINIMUM_IN_DEGREES = Projector.mercatorXToLongitude(-1.0)
+    static let LONGITUDE_DELTA_IN_DEGREES = LONGITUDE_MAXIMUM_IN_DEGREES - LONGITUDE_MINIMUM_IN_DEGREES
 }
